9c672f48304adb996a6c84f0e87c96a602c28746
cunfa <cunfamiao@163.com>
2019-09-10 16:56:06 +0800

    -> new file

---
+++ AGNetworking/AGNetworking/AGNetworking/AGHttpRequest.h
@@ -0,0 +1,17 @@
+//
+//  AGHttpRequest.h
+//  AGNetworking
+//
+//  Created by suning on 2019/9/10.
+//  Copyright © 2019 suning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AGHttpRequest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AGNetworking/AGNetworking/AGNetworking/AGHttpRequest.m
@@ -0,0 +1,13 @@
+//
+//  AGHttpRequest.m
+//  AGNetworking
+//
+//  Created by suning on 2019/9/10.
+//  Copyright © 2019 suning. All rights reserved.
+//
+
+#import "AGHttpRequest.h"
+
+@implementation AGHttpRequest
+
+@end
